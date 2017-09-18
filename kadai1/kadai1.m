27663f99b77ea620ce6620dd33dcb118b2346a5c
Oic5han <32050362+Oic5han@users.noreply.github.com>
2017-09-18 20:01:53 +0900

    コメントの編集

--- kadai1/kadai1.m
+++ kadai1/kadai1.m
@@ -7,31 +7,31 @@ imagesc(original); axis image;  % 画像の表示
 pause;                          % 一時停止
 
 % --- 縮小と拡大を行い画像を荒くしていく ---
-% 1回目
+% ピクセル数1/2
 IMG = imresize(original,0.5);   % 画像を0.5倍に縮小
 IMG2 = imresize(IMG,2,'box');   % 画像を2倍に拡大
 imagesc(IMG2); axis image;      % 画像の表示
 pause;                          % 一時停止
 
-% 2回目
+% ピクセル数1/4
 IMG = imresize(IMG,0.5);        % 画像を0.5倍に縮小
 IMG2 = imresize(IMG,4,'box');   % 画像を4倍に拡大
 imagesc(IMG2); axis image;      % 画像の表示
 pause;                          % 一時停止
 
-% 3回目
+% ピクセル数1/8
 IMG = imresize(IMG,0.5);        % 画像を0.5倍に縮小
 IMG2 = imresize(IMG,8,'box');   % 画像を8倍に拡大
 imagesc(IMG2); axis image;      % 画像の表示
 pause;                          % 一時停止
 
-% 4回目
+% ピクセル数1/16
 IMG = imresize(IMG,0.5);        % 画像を0.5倍に縮小
 IMG2 = imresize(IMG,16,'box');  % 画像を16倍に拡大
 imagesc(IMG2); axis image;      % 画像の表示
 pause;                          % 一時停止
 
-% 5回目
+% ピクセル数1/32
 IMG = imresize(IMG,0.5);        % 画像を0.5倍に縮小
 IMG2 = imresize(IMG,32,'box');  % 画像を32倍に拡大
 imagesc(IMG2); axis image;      % 画像の表示
